0e4f1b1d8b5032fbe329d9772bc4d09b024fd815
UncleCShark <38818698+UncleCShark@users.noreply.github.com>
2018-05-04 13:56:56 +0200

    Load Chapters 6 and 7

---
+++ Chapter07/ScaryColorList/ScaryColorList/ScaryColorList.NetStandard/ScaryColorListPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace ScaryColorList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScaryColorListPage : ContentPage
+    {
+        public ScaryColorListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Chapter07/TextVariations/TextVariations/TextVariations.NetStandard/TextVariationsPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TextVariations
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextVariationsPage : ContentPage
+    {
+        public TextVariationsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
